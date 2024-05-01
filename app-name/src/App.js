2ddda34dc8dc8ce7bcc97bbd4fd2644b1f440f65
Tianhui Zhou <tianhuizhou73@gmail.com>
2024-05-01 11:11:14 -0700

    start nav bar

---
+++ app-name/src/App.js
@@ -0,0 +1,36 @@
+function App() {
+  return (
+    <nav>
+    <button class="hamburgerMenu">
+        <span class="material-icons">menu</span>
+    </button>
+    
+    <ul>
+      <li class="navTitles"><a href="index.html">Home</a></li>
+      <li class="navTitles"><a href="order.html">Online Ordering</a></li>
+      <li class="navTitles"><a href="reservations.html">Reservations</a></li>
+      <li class="navTitles"><a href="order-popup.html">Order Pop Up</a></li>
+
+    </ul>
+  </nav>
+    
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+  );
+}
+
+export default App;
--- app-name/src/index.js
+++ app-name/src/index.js
@@ -13,3 +13,6 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+
+// In general, you’ll code React apps by implementing components in one or more modules(e.g., App.js, AboutPage.js), which will then be imported and used by the index.js file.See the Components section below for details (from textbook chapter15)

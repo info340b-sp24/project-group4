a818ea20d85b081068971c906e0c487d37a0fbfd
rachelhe <rachelxw@uw.edu>
2024-05-14 20:40:47 -0700

    d

--- app-name/src/ReservationsPage.js
+++ app-name/src/ReservationsPage.js
@@ -1,5 +1,30 @@
 import './index.css';
 
+export function ReservationsPage() {
+  return (
+    <div class="reservation-form">
+    <h1>Make a Reservation</h1>
+    <form action="/action_page.php">
+       <label for="fname">First name: </label>
+       <input type="text" id="fname" name="fname" placeholder="Tim"></input>
+       <label for="lname">Last name: </label>
+       <input type="text" id="lname" name="lname" placeholder="Carlson"></input>
+       <label for="date">Date</label>
+       <input type="text" id="date" name="date" placeholder="04/21/2024"></input>
+       <label for="time">Time</label>
+       <input type="text" id="time" name="time" placeholder="12:00"></input>
+       <label for="people">Number of People</label>
+       <input type="number" id="people" name="people" placeholder="2"></input>
+       <label for="email">Email:</label>
+       <input type="email" id="email" name="email" placeholder="txxxxx@uw.edu"></input>
+       <label for="phone">Phone Number:</label>
+       <input type="tel" id="phone" name="phone" placeholder="xxx-xxx-xxxx"></input>
+       <button type="submit">Submit</button>
+    </form>
+  </div>
+  )
+}
+
 document.addEventListener('DOMContentLoaded', function() {
     const reservationForm = document.getElementById('reservationForm');
     const reservationStatus = document.getElementById('reservationStatus');
@@ -46,9 +71,7 @@ document.addEventListener('DOMContentLoaded', function() {
         displayError('Please enter a valid phone number (10 digits).');
         return false;
       }
-  
-      // Additional validation for date and time can be added if needed
-      return true;
+        return true;
     }
   
     function displayError(message) {

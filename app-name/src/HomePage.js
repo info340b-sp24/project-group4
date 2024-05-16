62ef9e6cf507af5baf60704bf7adc0e0421d6c48
Tianhui Zhou <tianhuizhou73@gmail.com>
2024-05-16 16:39:32 -0700

    hamburger functional

--- app-name/src/HomePage.js
+++ app-name/src/HomePage.js
@@ -1,10 +1,12 @@
 import { HomeSlideshow } from './HomeSlideshow';
 import { Welcome } from './MainPageWelcome';
 import { ReviewSection } from './ReviewSection';
+import './HomePage.css';
+
 
 export function HomePage() {
     return (
-        <div>
+        <div class="HomePage">
             <HomeSlideshow />
             <Welcome />
             <ReviewSection />

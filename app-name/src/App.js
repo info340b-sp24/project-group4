67663f423431f5ab6d911f8ea91c44575b537421
Tianhui Zhou <tianhuizhou73@gmail.com>
2024-05-17 18:53:27 -0700

    tentative final draft 2 firebase

--- app-name/src/App.js
+++ app-name/src/App.js
@@ -8,7 +8,7 @@ export function App(props) {
         <div>
             <NavBar />
             <HomePage />
-            <Menu />
+            {/* <Menu /> */}
             {/* <ReservationsPage /> */}
         </div>
     )

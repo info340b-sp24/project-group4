0d614037c702ab431c36002e711c22c560501081
Tianhui Zhou <tianhuizhou73@gmail.com>
2024-05-23 19:48:37 -0700

    m

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -30,7 +30,6 @@ export function HomePage() {
                     <h1>Welcome to Ridge Cupbop and Ramen!</h1>
                 </div>
                 <div className='welcomeContainerInner' aria-label="a semi-opaque white rectangle">
-                    <p>About us... ~</p>
                     <p>We are committed to good food that will make you happy.</p>
                     <p>We hope to make your ordering, menu browsing, and reservations easier. And don't forget to see some of our customer reviews and photos, and submit your own below!</p>
                 </div>
